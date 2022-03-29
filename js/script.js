765e02baa6d07e75e7959500d8b3857c10a20e00
Naufal Adhi <71639940+Nopalogic@users.noreply.github.com>
2022-03-29 20:49:36 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector(".wrapper"),
+    qrInput = document.querySelector(".form input"),
+    generatorBtn = document.querySelector(".form button"),
+    qrImg = document.querySelector(".qr-code img");
+
+generatorBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value;
+    if (!qrValue) return;
+    generatorBtn.innerText = "Generating QR Code...";
+    qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=' + qrValue;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generatorBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value){
+        wrapper.classList.remove("active");
+    }
+});
