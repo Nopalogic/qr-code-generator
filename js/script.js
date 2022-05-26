85181359c690e1d733e4ba87a2e03c66a975a8a0
Naufal Adhi <71639940+Nopalogic@users.noreply.github.com>
2022-05-26 15:24:22 +0700

    mengubah single quote -> back tick pada link API

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ generatorBtn.addEventListener("click", () => {
     let qrValue = qrInput.value;
     if (!qrValue) return;
     generatorBtn.innerText = "Generating QR Code...";
-    qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=' + qrValue;
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data= ${qrValue}`;
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
         generatorBtn.innerText = "Generate QR Code";
